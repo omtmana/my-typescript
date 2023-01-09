968f55c339e320db8082e5ac9cc08565e4a4ac77
Olivia <oliviatmanalastas@gmail.com>
2023-01-09 17:44:56 -0500

    person object

--- app.js
+++ app.js
@@ -1,17 +1,5 @@
-function add_num(n1, n2) {
-    return n1 + n2;
-}
-var num1 = 5;
-var num2 = 2.8;
-var final_result = add_num(num1, num2);
-console.log(final_result);
-function combine_nums(n1, n2) {
-    if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-        throw new Error('Incorrect Input');
-    }
-    return n1 + n2;
-}
-var nums1 = 5;
-var nums2 = 2.8;
-var total = combine_nums(nums1, nums2);
-console.log(total);
+var person = {
+    name: 'Olivia',
+    age: '25'
+};
+console.log(person);
--- app.ts
+++ app.ts
@@ -1,22 +1,6 @@
-function add_num(n1: number, n2: number) {
-   return n1 + n2
-}
+const person = {
+   name: 'Olivia',
+   age: '25'
+};
 
-let num1 = 5
-let num2 = 2.8
-
-const final_result = add_num(num1, num2)
-console.log(final_result)
-
-function combine_nums(n1: number, n2: number) {
-   if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-      throw new Error('Incorrect Input')
-   }
-   return n1 + n2
-}
-
-let nums1 = 5
-let nums2 = 2.8
-
-const total = combine_nums(nums1, nums2)
-console.log(total)
+console.log(person)
